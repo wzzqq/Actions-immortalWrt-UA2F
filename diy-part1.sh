8a257f831e261fd21f27e64a620fd42e94e48610
wzzqq <40435421+wzzqq@users.noreply.github.com>
2023-06-21 14:53:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-git clone https://github.com/Zxilly/UA2F package/UA2F
+#git clone https://github.com/Zxilly/UA2F package/UA2F
+echo 'src-git UA2F https://github.com/Zxilly/UA2F.git' >> feeds.conf.default
